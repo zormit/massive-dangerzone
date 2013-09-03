9e309a69efa84f64e50df5723b5dfa48c41f2dc7
Moritz Neeb <moritz.neeb@stud.tu-darmstadt.de>
2013-09-03 22:08:20 +0200

    added loop
    the test will execute the whole loop, althoug a single assert failed.

--- http-forwarding-check/test.py
+++ http-forwarding-check/test.py
@@ -3,6 +3,7 @@ import unittest
 import csv
 from urllib.request import urlopen
 from http.client import HTTPConnection
+from urllib.error import HTTPError
 
 class TestDomainForwarding(unittest.TestCase):
     ''' this script tests, whether certain webserver-redirections do actually work
@@ -16,10 +17,21 @@ class TestDomainForwarding(unittest.TestCase):
     def test_all(self):
         ''' all in one test. could be extended to more than one test via
             https://github.com/txels/ddt but I think its overkill for now '''
-        for test in self.data:
-            response = urlopen(test['url'])
-            self.assertEqual(response.geturl(), test['forwarding'])
-            self.assertTrue(test['containing'] in str(response.read()))
+        success = True
+        for test_id, test in enumerate(self.data):
+            try:
+                response = urlopen(test['url'])
+            except HTTPError as e:
+                print(e)
+            try:
+                self.assertEqual(response.geturl(), test['forwarding'])
+                self.assertTrue(test['containing'] in str(response.read()))
+            except AssertionError as ass:
+                success = False
+                print('Test ID {} failed:{}'.format(test_id+1,ass))
+
+        if not success:
+            raise AssertionError("some of the url redirections did not work properly. see above, which one")
 
 if __name__ == '__main__':
     unittest.main()
